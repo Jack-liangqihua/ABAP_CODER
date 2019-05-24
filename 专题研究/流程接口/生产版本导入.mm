49ca8b5c7781ab2e4e6886095a19aa730591e41d
unknown <Administrator@xxglzx-lqh.keshun.com.cn>
2019-05-24 17:06:27 +0800

    Signed-off-by: unknown <Administrator@xxglzx-lqh.keshun.com.cn>

---
+++ 专题研究/流程接口/生产版本导入.mm
@@ -0,0 +1,41 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1558487803572" ID="ID_1195383271" MODIFIED="1558487893568" TEXT="&#x751f;&#x4ea7;&#x7248;&#x672c;&#x5bfc;&#x5165;">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <div>
+      <div>
+        <h5>
+          &#21518;&#26399;&#36816;&#32500;&#39035;&#30693;&#65306;
+        </h5>
+        <p>
+          &#22240;SAP&#21019;&#24314;&#29983;&#20135;&#29256;&#26412;&#65292;&#23454;&#38469;&#19978;&#26159;&#20351;&#29992;C223&#20107;&#29289;&#20195;&#30721;&#23545;MKAL&#34920;&#36827;&#34892;&#25554;&#20837;&#12289;&#21024;&#38500;&#31561;&#25805;&#20316;&#65292;&#33509;&#33258;&#21160;&#21019;&#24314;&#38169;&#35823;&#65292;&#21487;&#30452;&#25509;&#36827;&#34892;C223&#21024;&#38500;&#34892;&#39033;&#30446;&#12290;
+        </p>
+        <h5>
+          &#39069;&#22806;&#30693;&#35782;&#26469;&#28304;&#65306;
+        </h5>
+        <ul>
+          <li>
+            <p>
+              SAP-C223&#29983;&#20135;&#29256;&#26412;&#21019;&#24314;&#28041;&#21450;&#20989;&#25968;&#65306;<br />CM_FV_PROD_VERS_DB_UPDATE(&#23545;MKAL&#34920;&#30452;&#25509;&#25554;&#20837;&#21024;&#38500;&#31561;&#25805;&#20316;)<br />MATERIAL_UPDATE_ALL<br />MDIA_UPDATE_MKAL_INTERN
+            </p>
+          </li>
+        </ul>
+      </div>
+      <br />
+      &#160;
+    </div>
+    <p>
+      <a href="https://blogs.sap.com/2015/09/24/mass-production-version-creation/" target="_blank" rel="nofollow" style="background-color: rgb(255, 255, 255); color: rgb(49, 148, 208); text-decoration: none; font-family: -apple-system, SF UI Text, Arial, PingFang SC, Hiragino Sans GB, Microsoft YaHei, WenQuanYi Micro Hei, sans-serif; font-size: 16px; font-style: normal; font-weight: 400; letter-spacing: normal; text-align: left; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px"><font color="rgb(49, 148, 208)" face="-apple-system, SF UI Text, Arial, PingFang SC, Hiragino Sans GB, Microsoft YaHei, WenQuanYi Micro Hei, sans-serif" size="16px">https://blogs.sap.com/2015/09/24/mass-production-version-creation/</font></a>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1558487977800" ID="ID_1246679462" MODIFIED="1558487981280" POSITION="right" TEXT="BOM"/>
+<node CREATED="1558487981852" ID="ID_1785557248" MODIFIED="1558487984973" POSITION="right" TEXT="&#x5de5;&#x827a;&#x8def;&#x7ebf;"/>
+<node CREATED="1558487986876" ID="ID_438640824" MODIFIED="1558488006518" POSITION="right" TEXT="&#x8868;MKAL"/>
+</node>
+</map>
---
+++ 专题研究/流程接口/采购付款.mm
@@ -0,0 +1,13 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1558505464976" ID="ID_1814925606" MODIFIED="1558508533507" TEXT="&#x91c7;&#x8d2d;&#x4ed8;&#x6b3e;">
+<node CREATED="1558505945896" ID="ID_1116758053" MODIFIED="1558506633928" POSITION="right" TEXT="&#x91c7;&#x8d2d;PO+&#x5165;&#x5e93;&#x5355;&#x4fe1;&#x606f;+&#x53d1;&#x7968;&#x6821;&#x9a8c;">
+<node CREATED="1558506156580" ID="ID_1461561291" MODIFIED="1558506164842" TEXT="&#x63a8;&#x9001;OA&#x5ba1;&#x6279;&#x6d41;&#x7a0b;"/>
+</node>
+<node CREATED="1558506127909" ID="ID_1824556575" MODIFIED="1558506132065" POSITION="right" TEXT="&#x4f9b;&#x5e94;&#x5546;&#x9884;&#x4ed8;&#x6b3e;">
+<node CREATED="1558506188446" ID="ID_415838014" MODIFIED="1558506215525" TEXT="&#x51cf;&#x53bb;&#x9884;&#x4ed8;&#x6b3e;&#x7684;&#x6570;&#x636e;&#xff0c;&#x5269;&#x4f59;&#x90e8;&#x5206;&#x505a;&#x53d1;&#x7968;&#x6821;&#x9a8c;"/>
+</node>
+<node CREATED="1558508286636" ID="ID_1926828784" MODIFIED="1558508293238" POSITION="right" TEXT="&#x9884;&#x4ed8;&#x6b3e;&#x8f6c;&#x79fb;"/>
+<node CREATED="1558508585795" ID="ID_95755078" MODIFIED="1558508604491" POSITION="right" TEXT="OA&#x5ba1;&#x6279;&#x540e;&#x4ea7;&#x751f;&#x8d22;&#x52a1;&#x4f1a;&#x8ba1;&#x51ed;&#x8bc1;"/>
+</node>
+</map>
